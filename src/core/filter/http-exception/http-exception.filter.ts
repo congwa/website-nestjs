dfa83020e6385ad9f19d6370d773dbd9e76c7b85
cong_wa <cong_wa@163.com>
2023-04-27 18:51:43 +0800

    build: feat

--- src/core/filter/http-exception/http-exception.filter.ts
+++ src/core/filter/http-exception/http-exception.filter.ts
@@ -12,10 +12,42 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp(); // 获取请求上下文
     const response = ctx.getResponse(); // 获取请求上下文中的 response对象
     const status = exception.getStatus(); // 获取异常状态码
-    // TODO 这里response是私有的，不知道该咋写了
+    // 这里response是私有的，只能这样
+
     const exceptionStr = JSON.stringify(exception)
     const exceptionJSON = JSON.parse(exceptionStr);
     Logger.error(exceptionStr);
+    /**
+      // 还可以继承ValidationPipe管道，对handleError方法重写，整理message的信息
+       handleError(errors: ValidationError[]) {
+      const updatedErrors = errors.map((error) => {
+        const constraints = error.constraints;
+        // 添加 ID 信息
+        return {
+          ...error,
+          property: `${error.property}[${error.value.id}]`,
+          constraints: Object.keys(constraints).reduce((prev, key) => {
+            return {
+              ...prev,
+              [key]: `${constraints[key]} [ID:${error.value.id}]`,
+            };
+          }, {}),
+        };
+      });
+      return super.handleError(updatedErrors);
+    }
+
+    buildMessage(errors: ValidationError[]) {
+      const messages = errors.map((error) => {
+        const constraints = error.constraints;
+        // 在错误信息中包含 ID 信息
+        return Object.keys(constraints)
+          .map((key) => constraints[key])
+          .join('; ');
+      });
+      return messages.join('; ');
+    }
+     */
 
     // 设置错误信息
     const message = exceptionJSON?.response?.message
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,17 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     // Request Validation
+    // ValidationPipe 是 Nest.js 中的一个内置管道之一，主要用于验证客户端请求的数据。它基于 class-validator 库来实现，并支持声明式验证装饰器。
+    /**
+        skipMissingProperties: 是否跳过缺失的属性，默认为 false。
+        transform: 是否在验证前将输入数据转换为一个类实例，默认为 true。
+        whitelist: 是否仅验证 DTO 声明中定义的属性，默认为 true。
+        forbidNonWhitelisted: 如果设置为 true，则不允许验证 DTO 声明以外的任何属性，默认为 true。
+        groups: 指定要使用的分组，仅验证与指定分组相关联的验证装饰器，默认为 null。
+        disableErrorMessages: 是否禁用错误消息的生成，默认为 false。
+        transformOptions: 一个可选的对象，其中包含需要发送到 class-transformer 的转换选项。
+        exceptionFactory: 可选的函数，用于自定义异常的实例化过程。
+     */
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
